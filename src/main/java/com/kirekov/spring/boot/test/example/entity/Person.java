8b16a46123eb930d12c5e8bd11bd4f354b51171e
Semyon Kirekov <semyon@kirekov.com>
2021-06-12 17:26:27 +0300

    Add 'Person' entity

---
+++ src/main/java/com/kirekov/spring/boot/test/example/entity/Person.java
@@ -0,0 +1,53 @@
+package com.kirekov.spring.boot.test.example.entity;
+
+
+import java.time.ZonedDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import lombok.Getter;
+
+/**
+ * Person table.
+ */
+@Entity
+@Table(name = "person")
+@Getter
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private ZonedDateTime dateCreated;
+
+  @PrePersist
+  void prePersist() {
+    dateCreated = ZonedDateTime.now();
+  }
+
+  public Person setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public Person setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public Person setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+}
