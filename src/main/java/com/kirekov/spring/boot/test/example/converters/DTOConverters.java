763a50b85fdc614aa3bbe96f0ec2061c12851576
Semyon Kirekov <semyon@kirekov.com>
2021-06-12 17:29:01 +0300

    Add DTOConverters.toPersonDTO

---
+++ src/main/java/com/kirekov/spring/boot/test/example/converters/DTOConverters.java
@@ -0,0 +1,15 @@
+package com.kirekov.spring.boot.test.example.converters;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import com.kirekov.spring.boot.test.example.dto.PersonDTO;
+import com.kirekov.spring.boot.test.example.entity.Person;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class DTOConverters {
+
+  public static PersonDTO toPersonDTO(Person person) {
+    return new PersonDTO(person.getId(), person.getFirstName(), person.getLastName(), person.getDateCreated());
+  }
+}
