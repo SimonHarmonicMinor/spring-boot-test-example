11df21a01250eddb6a62b4236b5c88e4f90204be
Semyon Kirekov <semyon@kirekov.com>
2021-06-12 15:43:28 +0300

    init

---
+++ src/main/java/com/kirekov/spring/boot/test/example/SpringBootTestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kirekov.spring.boot.test.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestExampleApplication.class, args);
+	}
+
+}
