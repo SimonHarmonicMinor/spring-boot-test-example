fd30e9c2846637a4b79d07faf3f6713003e6f62b
Semyon Kirekov <semyon@kirekov.com>
2021-06-12 15:57:20 +0300

    fixed SpringBootTestExampleApplication according to checkstyle rules

--- src/main/java/com/kirekov/spring/boot/test/example/SpringBootTestExampleApplication.java
+++ src/main/java/com/kirekov/spring/boot/test/example/SpringBootTestExampleApplication.java
@@ -3,11 +3,14 @@ package com.kirekov.spring.boot.test.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point.
+ */
 @SpringBootApplication
 public class SpringBootTestExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTestExampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTestExampleApplication.class, args);
+  }
 
 }
