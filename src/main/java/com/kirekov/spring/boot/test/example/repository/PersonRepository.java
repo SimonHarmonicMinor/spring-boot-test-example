8de6b7157931ebd650fe3bf7ddc7b1784d9b1443
Semyon Kirekov <semyon@kirekov.com>
2021-06-15 22:26:12 +0300

    Added PersonRepository.findAllLastNames + unit DataJpaTest

--- src/main/java/com/kirekov/spring/boot/test/example/repository/PersonRepository.java
+++ src/main/java/com/kirekov/spring/boot/test/example/repository/PersonRepository.java
@@ -1,7 +1,9 @@
 package com.kirekov.spring.boot.test.example.repository;
 
 import com.kirekov.spring.boot.test.example.entity.Person;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * DAO for {@linkplain Person}.
@@ -9,4 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
   boolean existsByFirstNameAndLastName(String firstName, String lastName);
+
+  @Query("select distinct p.lastName from Person p")
+  Set<String> findAllLastNames();
 }
---
+++ src/test/java/com/kirekov/spring/boot/test/example/repository/PersonRepositoryDataJpaTest.java
@@ -0,0 +1,27 @@
+package com.kirekov.spring.boot.test.example.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.kirekov.spring.boot.test.example.entity.Person;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@DisplayName("PersonRepository: findAllLastNames tests")
+class PersonRepositoryDataJpaTest {
+  @Autowired
+  private PersonRepository personRepository;
+
+  @Test
+  @DisplayName("Should return all last names")
+  void shouldReturnAlLastNames() {
+    personRepository.saveAndFlush(new Person().setFirstName("John").setLastName("Brown"));
+    personRepository.saveAndFlush(new Person().setFirstName("Kyle").setLastName("Green"));
+    personRepository.saveAndFlush(new Person().setFirstName("Paul").setLastName("Brown"));
+
+    assertEquals(Set.of("Brown", "Green"), personRepository.findAllLastNames());
+  }
+}
