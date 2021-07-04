207bc50c67f2cf639ec41d898367615d3998f37c
Semyon Kirekov <semyon@kirekov.com>
2021-07-04 13:29:12 +0700

    Added TestProfiles.TEST_CONTAINERS

---
+++ src/test/java/com/kirekov/spring/boot/test/example/TestProfiles.java
@@ -0,0 +1,9 @@
+package com.kirekov.spring.boot.test.example;
+
+public final class TestProfiles {
+
+  private TestProfiles() {
+  }
+
+  public static final String TEST_CONTAINERS = "test-containers";
+}
