1aadf45ed6d43466e2001adf31de689b3f02459d
Semyon Kirekov <semyon@kirekov.com>
2021-07-05 18:16:25 +0700

    Added PersonRepositoryTestContainers

---
+++ src/test/java/com/kirekov/spring/boot/test/example/repository/PersonRepositoryTestContainers.java
@@ -0,0 +1,36 @@
+package com.kirekov.spring.boot.test.example.repository;
+
+import static com.kirekov.spring.boot.test.example.TestProfiles.TEST_CONTAINERS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.kirekov.spring.boot.test.example.entity.Person;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@DataJpaTest
+@Testcontainers
+@ActiveProfiles(TEST_CONTAINERS)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@DisplayName("PersonRepository: findAllLastNames tests with PostgreSQL Testcontainers instance")
+class PersonRepositoryTestContainers {
+
+  @Autowired
+  private PersonRepository personRepository;
+
+  @Test
+  @DisplayName("Should return all last names")
+  void shouldReturnAlLastNames() {
+    personRepository.saveAndFlush(new Person().setFirstName("John").setLastName("Brown"));
+    personRepository.saveAndFlush(new Person().setFirstName("Kyle").setLastName("Green"));
+    personRepository.saveAndFlush(new Person().setFirstName("Paul").setLastName("Brown"));
+
+    assertEquals(Set.of("Brown", "Green"), personRepository.findAllLastNames());
+  }
+}
