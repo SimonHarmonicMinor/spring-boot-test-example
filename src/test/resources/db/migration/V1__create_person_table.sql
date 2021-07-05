a396ed7c73a33edcd35de5dc82b083643096bf8c
Semyon Kirekov <semyon@kirekov.com>
2021-07-05 19:20:57 +0700

    Fixed migration formatting

--- src/test/resources/db/migration/V1__create_person_table.sql
+++ src/test/resources/db/migration/V1__create_person_table.sql
@@ -1,7 +1,7 @@
 create table person
 (
-    id          serial primary key,
-    first_name  text,
-    last_name   text,
+    id           serial primary key,
+    first_name   text,
+    last_name    text,
     date_created timestamp with time zone
 );
